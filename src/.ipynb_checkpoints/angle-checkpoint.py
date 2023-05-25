ad6f58a65657038f6749eb8ad221ef994423eb4c
Dominik Poch <>
2023-05-25 13:50:33 +0200

    Remove jupyter files

--- src/.ipynb_checkpoints/angle-checkpoint.py
+++
@@ -1,2 +0,0 @@
-def compute_angle():
-    # vypočítat úhel dvou přímek
--- src/.ipynb_checkpoints/preprocessing-checkpoint.py
+++
@@ -1,7 +0,0 @@
-from skimage.color import rgb2gray
-from skimage.exposure import rescale_intensity
-
-
-def convert_to_grayscale(img):
-    grayscale = rgb2gray(img) * 255
-    return rescale_intensity(grayscale, out_range=(0, 255))
